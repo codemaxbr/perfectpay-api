8b89c4f76c2e4e3dd64b254050f0f73ee2e72f3a
Lucas Maia <lucas.codemax@gmail.com>
2024-01-03 16:42:51 -0300

    Ajuste CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,9 +79,9 @@ $app->configure('database');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
